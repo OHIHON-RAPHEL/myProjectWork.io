9eae8faa124f7ed1ea8fd1cb4f190036a30e27ae
ohihon raphel <ohihonraphel@gmail.com>
2025-06-15 00:01:39 -0700

    Recently

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '../images/banner.gif';
 
+
 const Banner = () => {
   return (
     <div className="bg-amber-500 p-3 flex justify-center">
@@ -17,6 +18,7 @@ const Banner = () => {
             className="w-full h-auto object-contain"
           />
         </a>
+        
         
       </div>
     </div>
